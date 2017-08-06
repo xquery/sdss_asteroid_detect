c72c21ec3ec3b499870a1b98aa3dd4fb9fe2e67f
xquery <jim@webcomposite.com>
2017-08-06 11:51:51 +0200

    update tests with positives

--- test/unit_test_naive_detect.cpp
+++ test/unit_test_naive_detect.cpp
@@ -3,14 +3,10 @@
 
 using namespace std;
 
-//TEST(DetectTest,TestNaiveDetectNonExistentImageFile){
-//    EXPECT_EQ(ad::naive_detect("adfasdfadfadfnon-existent-image-file.jpg"),-1);
-//}
-
 TEST(DetectTest,TestNaiveDetectNoHits){
-    EXPECT_EQ(ad::naive_detect("../data/test/test3.jpg"),0);
+    EXPECT_EQ(ad::naive_detect("../data/test/positives/test.jpg"),0);
 }
 
 TEST(DetectTest,TestNaiveDetectHits){
-    EXPECT_EQ(ad::naive_detect("../data/test/test.jpg"),0);
+    EXPECT_EQ(ad::naive_detect("../data/test/positives/test6-positive.jpg"),0);
 }
