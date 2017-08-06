0d1e663e2771ed56c7427a9de0b73e45d4732837
xquery <jim@webcomposite.com>
2017-08-06 13:18:07 +0200

    publish results to

--- data/asteroid-detect.sh
+++ data/asteroid-detect.sh
@@ -2,7 +2,7 @@
 
 search=${1}
 
-find images -name "*4927*.jpg" | while read file; do
+find images -name "*.jpg" | while read file; do
 echo "processing $file"
 ../cmake-build-debug/naive_detect $file
 done
